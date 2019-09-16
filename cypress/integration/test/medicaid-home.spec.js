802bc9fd96dc90d4c11b0b404e849585b9cc9c16
NirmalAutomation <35673592+NirmalAutomation@users.noreply.github.com>
2019-09-15 21:18:17 -0400

    commit-message

--- cypress/integration/test/medicaid-home.spec.js
+++ cypress/integration/test/medicaid-home.spec.js
@@ -9,23 +9,36 @@ describe("Testing Mediciad.gov", () => {
 
         cy.visit("/?ACA=JMwCeGV1O8");
 
-        cy.visit('/use-multiple-concurrent-antipsychotics-children-adolescents');
+        cy.visit('/follow-after-hospitalization-mental-illness-ages-6-20');
 
-        cy.xpath('//*[@id="block-medicaid-content"]/div/article/div[4]/div[5]')
-            .should('contain', 'State')
-            .and('contain', 'Rate')
-            .and('contain', 'Median - 2.9%')
+        // cy.xpath('//*[@id="block-medicaid-content"]/div/article/div[4]/div[5]')
+        //     .should('contain', 'State')
+        //     .and('contain', 'Rate')
+        //     .and('contain', 'Median - 2.9%')
 
-        cy.get('.chart-axisTop')
-            .should('contain', '0%')
-            .and('contain', '25%')
+        // cy.get('.chart-axisTop')
+        //     .should('contain', '0%')
+        //     .and('contain', '25%')
 
-        cy.get('.chart-axisBottom')
-            .should('contain', '0%')
-            .and('contain', '25%')
+        // cy.get('.chart-axisBottom')
+        //     .should('contain', '0%')
+        //     .and('contain', '25%')
 
-        cy.get('.chart-axisRight')
-            .contains('NR')
-            .should('have.css', 'color', 'rgb(2, 102, 102)');
+        // cy.get('.chart-axisRight')
+        //     .contains('NR')
+        //     .should('have.css', 'color', 'rgb(2, 102, 102)');
+
+        // cy.get('.bar-alt')
+        //     .should('have.css', 'fill', 'rgb(136, 136, 136)');
+
+        // cy.get('.bar')
+        //     .should('have.css', 'fill', 'rgb(23, 65, 95)');
+
+        // cy.get('.bar-top.bar-top-population-1')
+        //     .should('have.css', 'fill', 'rgb(2, 102, 102)');
+
+        cy.get('.radio-button-text')
+            .contains('30-Day Follow-Up Rate')
+            .click();
     })
 })
